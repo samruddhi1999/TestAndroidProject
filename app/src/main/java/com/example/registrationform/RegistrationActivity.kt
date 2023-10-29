68def40a097001d4f37b4a1a259e7ecb5378e08a
Samruddhi Kedare <samruddhikedare@gmail.com>
2023-10-29 20:47:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/registrationform/RegistrationActivity.kt
@@ -0,0 +1,17 @@
+package com.example.registrationform
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+
+
+}
